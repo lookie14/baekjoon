f5a6db4a06dd13b8a8e40adecc2d4d6bb03d63b9
lookie14 <66856155+lookie14@users.noreply.github.com>
2023-02-24 17:11:34 +0900

    [Bronze V] Title: A+B - 4, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+int main() {
+    int a  , b ;
+    while(scanf("%d %d", &a, &b) != EOF) {
+        cout << a + b << endl;
+    }
+    return 0;
+}
