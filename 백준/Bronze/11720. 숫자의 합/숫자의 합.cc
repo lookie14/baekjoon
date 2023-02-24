d4be513d526ecc7773561442a7182ccdfb187588
lookie14 <66856155+lookie14@users.noreply.github.com>
2023-02-24 21:04:43 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    string str;
+    cin >> str;
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum = sum + str[i]-'0';
+    }
+    cout << sum;
+    return 0;
+}
