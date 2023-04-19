693513b3c5282709e7efffb8ed9345f84c8d29d6
lookie14 <66856155+lookie14@users.noreply.github.com>
2023-04-19 09:30:16 +0900

    [Bronze I] Title: 더하기 사이클, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+	int init, N;
+	int count = 0;
+	cin >> init;
+	N = init;
+	do {
+		N = (N % 10) * 10 + ((N / 10) + (N % 10)) % 10;
+		count++;
+	} while (init != N);
+	cout << count;
+	return 0;
+}
