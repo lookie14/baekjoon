46ee52129f2253577143a5db2c0391d50b19ac18
lookie14 <66856155+lookie14@users.noreply.github.com>
2024-03-11 18:50:46 +0900

    [Silver III] Title: 주유소, Time: 12 ms, Memory: 2800 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int arr1[100001]; //주유소
+int arr2[100001]; //도로
+int ans = 0;
+int mini;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N; cin >> N;
+    for(int i = 0 ; i < N-1; i++){
+        cin >> arr2[i];
+    }
+    for(int i = 0 ; i < N; i++){
+        cin >> arr1[i];
+        if(i ==0) mini = arr1[i];
+    }
+
+    ans += arr1[0]*arr2[0];
+    for(int i = 1; i < N-1; i++){
+        if(arr1[i] > mini){
+            ans += mini*arr2[i];
+        }else{
+            ans += arr2[i]*arr1[i];
+            mini = arr1[i];
+        }
+    }
+    cout << ans;
+    
+    return 0;
+}
